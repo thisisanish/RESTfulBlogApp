fcc2398e18bc40ff6483efcbcb6af236e4dab978
anish <email.anishagarwal@gmail.com>
2018-08-19 21:52:40 +0530

    major changes

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var express = require("express"),
     app=express(),
     mongoose = require("mongoose"),
     methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
     bodyParser = require("body-parser");
+    
 
 
 mongoose.connect("mongodb://localhost/restful_blog_app")
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride("_method"))
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+// Schema
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -24,9 +29,14 @@ var Blog = mongoose.model("Blog", blogSchema);
 //     image: "https://www.revenuearchitects.com/wp-content/uploads/2017/02/Blog_pic.png",
 //     body: "bodybodybodybodybodybodybodybodybodybodybody"
 // });
+
+
+// Root
 app.get("/", function(req, res){
     res.redirect("/blogs")
 })
+
+// Index
 
 app.get("/blogs", function (req, res){
     Blog.find({}, function(err, blogs){
@@ -37,7 +47,19 @@ app.get("/blogs", function (req, res){
         }
     })
 })
+
+
+//NEW
+
+app.get("/blogs/new", function(req, res){
+    res.render("new")
+})
+
+
+// Create
+
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             console.log(err);
@@ -47,12 +69,9 @@ app.post("/blogs", function(req, res){
     })
 })
 
-//NEW
-app.get("/blogs/new", function(req, res){
-    res.render("new")
-})
 
 //Show
+
 app.get("/blogs/:id", function(req, res){
     Blog.findById(req.params.id, function(err, foundBlog){
         if(err){
@@ -79,6 +98,7 @@ app.get("/blogs/:id/edit", function(req, res){
 // Update
 
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog){
         if(err){
             res.redirect("/blogs")
@@ -88,8 +108,19 @@ app.put("/blogs/:id", function(req, res){
     })
 })
 
+// Delete
 
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs")
+        }else{
+            res.redirect("/blogs")
+        }
+    })
+})
 
+// Listening 
 app.listen(3000, function(){
     console.log("running on 3000")
 })
---
+++ task.md
@@ -0,0 +1,13 @@
+# TO-DO
+
+    Make modules and regroup things.
+
+    Remove semantic UI and add Bootstrap.
+
+    Rebuild the code in a more functional way.
+
+    Add Authentication and learn more about pasport js.
+
+    Shift from Passportjs to token based auth.
+
+    Rebuild the code using promises.
